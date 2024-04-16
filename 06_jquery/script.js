e3fa00bda15ef1164dd5418d8f0363565a7e129c
KateMcRo <katemcr0@gmail.com>
2024-04-16 16:16:43 -0700

    removed solution

--- 06_jquery/script.js
+++ 06_jquery/script.js
@@ -3,12 +3,8 @@ function test() {
 }
 // jQuery function that can only run after the page is fully loaded
 
-$(document).ready(function () {
-  // Target button and create a click function
-  $("#modeToggle").click(function () {
-    // Target the body and toggle the class "dark-mode"
-    $("body").toggleClass("dark-mode");
-  });
-});
+// Target button and create a click function
+
+// Target the body and toggle the class "dark-mode"
 
 test();
