6dd5ff8dfd0ea481b5f5f7590edfe1163dc3d849
KateMcRo <katemcr0@gmail.com>
2024-04-16 16:06:02 -0700

    minor updates for clarity

--- 06_jquery/script.js
+++ 06_jquery/script.js
@@ -1,5 +1,14 @@
+function test() {
+  console.log("working");
+}
 // jQuery function that can only run after the page is fully loaded
+
 $(document).ready(function () {
   // Target button and create a click function
-  // Target the body and toggle the class "dark-mode"
+  $("#modeToggle").click(function () {
+    // Target the body and toggle the class "dark-mode"
+    $("body").toggleClass("dark-mode");
+  });
 });
+
+test();
